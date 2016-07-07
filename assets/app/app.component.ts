70dca6840fbabe68fc7d972f92952a460b93cb67
Devin Henkel <devin@devinhenkel.com>
2016-07-06 22:07:17 -0500

    message array

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -9,5 +9,10 @@ import {Message} from './messages/message';
     directives: [MessageComponent]
 })
 export class AppComponent {
-  message:Message = new Message('Flibbity floo! Deep Dudu!!!', null, 'D Dogg');
+  messages:Message[] = [
+    new Message('Flibbity floo! Deep Dudu!!!', null, 'D Dogg'),
+    new Message('What up, D Dogg Diggity Dogg Daddy Daddy Dogg Dogg', null, 'Klumpy'),
+    new Message("I'm not going to spy on you", null, 'Deep Dudu'),
+    new Message('Have you ever seen the rain?', null, 'John Fogarty')
+  ]
 }
